f2cc9ac4d3c28fae28912b35230181328d8907ef
samueldibella <samuel.dibella@gmail.com>
2017-07-15 18:44:23 -0400

    Early colophon notes

--- src/colophon.html.pm
+++ src/colophon.html.pm
@@ -1,1 +1,3 @@
 #lang pollen
+
+Set in IM Fell French Canon.
--- src/designernote.html.pm
+++ src/designernote.html.pm
@@ -1,4 +1,6 @@
 #lang pollen
 
+This layout of Edwin A. Abbott's Flatland: A Romance of Many Dimensions was designed by Samuel DiBella.
+
 Rights and Reproductions
 «The Fell Types are digitally reproduced by Igino Marini. www.iginomarini.com»
